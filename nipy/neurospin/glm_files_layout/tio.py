c720c12ca46619348f2e36434972ff018ea56d38
bthirion <bertrand.thirion@inria.fr>
2010-09-02 18:28:23 +0200

    propagated debugging in tio that occurred in master branch

--- nipy/neurospin/glm_files_layout/tio.py
+++ nipy/neurospin/glm_files_layout/tio.py
@@ -97,9 +97,9 @@ class Texture(object):
             #
             # TODO some sanity check on data length
             p.data = []
-            
             for t in range(nb_t):
-                tmp = (_np.frombuffer(f_in.read(4), _np.uint32))
+                # go forward in the buffer
+                f_in.read(4)
                 nbitems = (_np.frombuffer(f_in.read(4), _np.uint32))[0]
                 size = nbitems*datatype().nbytes
                 p.data.append(_np.frombuffer(f_in.read(size),datatype))
